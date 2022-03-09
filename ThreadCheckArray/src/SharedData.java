2fce4f1f748535d6781258a55f4b3c28aff3bd8f
HallelWeil <HallelWeil@gmail.com>
2022-03-09 10:58:53 +0200

    3. add javadoc comments

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,47 +1,87 @@
 import java.util.ArrayList;
 
-public class SharedData 
-{
-	private ArrayList<Integer> array;
-	private boolean [] winArray;
-	private boolean flag;
-	private final int b;
-	
+/**
+ * Description of SharedData save all the shared data for the threads
+ */
+public class SharedData {
 	/**
-	 * @param array
-	 * @param b
+	 * description of array the input array
+	 */
+	private ArrayList<Integer> array;
+	/**
+	 * description of winArray boolean array, true if the number in the same index
+	 * is in the result
+	 */
+	private boolean[] winArray;
+	/**
+	 * description of flag synchronized, true if a thread found result
+	 */
+	private boolean flag;
+	/** b - the number we try to get */
+	private final int b;
+
+	/**
+	 * constructor, get the input array and the num to get
+	 * 
+	 * @param array the input array
+	 * @param b     the result
 	 */
 	public SharedData(ArrayList<Integer> array, int b) {
-		
+
 		this.array = array;
 		this.b = b;
 	}
-	
-	public boolean[] getWinArray() 
-	{
+
+	/**
+	 * return the WinArray
+	 * 
+	 * @return the WinArray
+	 */
+	public boolean[] getWinArray() {
 		return winArray;
 	}
 
-	public void setWinArray(boolean [] winArray) 
-	{
+	/**
+	 * set the win array
+	 * 
+	 * @param winArray
+	 */
+	public void setWinArray(boolean[] winArray) {
 		this.winArray = winArray;
 	}
 
-	public ArrayList<Integer> getArray() 
-	{
+	/**
+	 * return the input array
+	 * 
+	 * @return return the array
+	 */
+	public ArrayList<Integer> getArray() {
 		return array;
 	}
 
-	public int getB() 
-	{
+	/**
+	 * return b
+	 * 
+	 * @return return b
+	 */
+	public int getB() {
 		return b;
 	}
 
-	public boolean getFlag() 
-	{
+	/**
+	 * return the flag
+	 * 
+	 * @return return the flag
+	 */
+	public boolean getFlag() {
 		return flag;
 	}
 
+	/**
+	 * set the flag
+	 * 
+	 * @param flag
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
